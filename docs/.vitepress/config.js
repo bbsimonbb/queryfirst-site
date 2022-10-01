126675af9ef82d982b37d05aecbeaaa608b4d2ed
bbsimonbb <bbsimonbb@gmail.com>
2022-10-01 19:07:32 +0200

    Modify gitignore

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,19 @@
+export default {
+  title: "QueryFirst",
+  description: "Frictionless data access for C# projects",
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/' },
+          { text: 'Getting Started', link: '/getting-started' },
+          {text: 'Authoring SQL', link: '/authoring-sql'},
+          {text: 'The Generated Code', link:'/generated'},
+          {text: 'Extending QueryFirst', link:'/extending'},
+          {text: 'The SelfTest() Method', link:'/selftest'}
+        ]
+      }
+    ]
+  }
+}
