bb25849899c0a4630f1100678b5628e636e716ed
Simon Boddy <bbsimonbb@gmail.com>
2026-03-13 15:59:49 +0100

    pipeline

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,20 +1,21 @@
 export default {
   title: "QueryFirst",
   description: "Frictionless data access for C# projects",
+  base: "/queryfirst-site/",
   themeConfig: {
     sidebar: [
       {
-        text: 'Guide',
+        text: "Guide",
         items: [
-          { text: 'Introduction', link: '/' },
-          { text: 'V2 Breaking Changes', link: '/v2-breaking-changes' },
-          { text: 'Getting Started', link: '/getting-started' },
-          {text: 'Authoring SQL', link: '/authoring-sql'},
-          {text: 'The Generated Code', link:'/generated'},
-          {text: 'Extending QueryFirst', link:'/extending'},
-          {text: 'The SelfTest() Method', link:'/selftest'}
-        ]
-      }
-    ]
-  }
-}
+          { text: "Introduction", link: "/" },
+          { text: "V2 Breaking Changes", link: "/v2-breaking-changes" },
+          { text: "Getting Started", link: "/getting-started" },
+          { text: "Authoring SQL", link: "/authoring-sql" },
+          { text: "The Generated Code", link: "/generated" },
+          { text: "Extending QueryFirst", link: "/extending" },
+          { text: "The SelfTest() Method", link: "/selftest" },
+        ],
+      },
+    ],
+  },
+};
